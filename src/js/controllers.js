9b07feca71fdde9765e052142a206c1cb4de630c
Alex Johnson <alex.johnson@evertrue.com>
2014-05-13 17:02:06 -0400

    Remove unused files

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -6,17 +6,17 @@ var phonecatControllers = angular.module('phonecatControllers', []);
 
 phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
   function($scope, Phone) {
-    $scope.phones = Phone.query();
+    $scope.users = Phone.query();
     $scope.orderProp = 'age';
   }]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
+// phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+//   function($scope, $routeParams, Phone) {
+//     $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+//       $scope.mainImageUrl = phone.images[0];
+//     });
 
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    }
-  }]);
+//     $scope.setImage = function(imageUrl) {
+//       $scope.mainImageUrl = imageUrl;
+//     }
+//   }]);
--- src/js/services.js
+++ src/js/services.js
@@ -7,6 +7,6 @@ var phonecatServices = angular.module('phonecatServices', ['ngResource']);
 phonecatServices.factory('Phone', ['$resource',
   function($resource){
     return $resource('data/:phoneId.json', {}, {
-      query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+      query: {method:'GET', params:{phoneId:'users'}, isArray:true}
     });
   }]);
